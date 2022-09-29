ed30d44755faa6de3fa714b33b8083717569b5c5
Sasha Petrenko <sap625@umsystem.edu>
2022-09-29 13:58:43 -0500

    Dev testing

--- src/aliases.jl
+++ src/aliases.jl
@@ -41,3 +41,20 @@ const RealFP = Union{Float32, Float64}
 
 # System's largest native floating point variable
 const Float = (Sys.WORD_SIZE == 64 ? Float64 : Float32)
+
+
+# # Real-numbered aliases
+# const RealArray{T<:Real, N} = AbstractArray{T, N}
+# const RealVector{T<:Real} = AbstractArray{T, 1}
+# const RealMatrix{T<:Real} = AbstractArray{T, 2}
+
+# # Integered aliases
+# const IntegerArray{T<:Integer, N} = AbstractArray{T, N}
+# const IntegerVector{T<:Integer} = AbstractArray{T, 1}
+# const IntegerMatrix{T<:Integer} = AbstractArray{T, 2}
+
+# # Specifically floating-point aliases
+# const RealFP = Union{Float32, Float64}
+
+# # System's largest native floating point variable
+# const Float = (Sys.WORD_SIZE == 64 ? Float64 : Float32)
--- test/test_sets.jl
+++ test/test_sets.jl
@@ -1,7 +1,22 @@
 using ScientificTypesAliases
 using Test
 
-@testset "OptimalEstimation" begin
+using Logging
+
+@testset "ScientificTypesAliases" begin
     ScientificTypesAliases.greet()
-    println(ScientificTypesAliases.STA_VERSION)
+    @info ScientificTypesAliases.STA_VERSION
+end
+
+@testset "Functions" begin
+
+    function test_types(a::Continuous)
+        print(typeof(a))
+    end
+
+    a = 0
+    @test_throws ErrorException test_types(a)
+    b = 5.1
+    test_types(b)
+
 end
